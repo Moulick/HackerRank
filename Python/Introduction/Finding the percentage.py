6f7c9dce201b2390f83d146f3073bb9e898ebac2
Moulick Aggarwal <byesahil+github@gmail.com>
2016-07-05 16:45:58 +0530

    'fourth'

--- Python/Introduction/Finding the percentage.py
+++ Python/Introduction/Finding the percentage.py
@@ -1,10 +1,17 @@
 student_number = int(input())
 
 marks_dict = {}
-total = 0
-
+total = 0.00
+dem = 0.00
 for i in range(student_number):
     x = input()
     x_1 = x.split()
     marks_dict[x_1[0]] = x_1[1:4]
-    print(marks_dict)
+
+name = str(input())
+if name in marks_dict:
+    for i in marks_dict[name]:
+        total = total + float(i)
+        dem = dem + 1
+avg = total / dem
+print("%.2f" % avg)
