32d0db80d697e7fe995b7cccbcb7b94df44ff567
Moulick <byesahil+github@gmail.com>
2016-07-14 00:32:37 +0530

    Day 7 correction

--- 30-Days-of-Code/Day 7 Arrays.py
+++ 30-Days-of-Code/Day 7 Arrays.py
@@ -2,5 +2,7 @@
 
 import sys
 
-n = int(input())
-print(''.join(input()))
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+print(" ".join(str(x) for x in arr[::-1]))
