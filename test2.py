b5e755f542124e9fc20c5f4e2c5868183325a658
Moulick <byesahil+github@gmail.com>
2016-07-15 23:58:28 +0530

    finished strings

--- test2.py
+++ test2.py
@@ -1,3 +1,17 @@
-n = int(input())
-name_numbers = [input().split() for _ in range(n)]
-print(name_numbers)
+s = raw_input()
+lenght = len(s)
+kev_points = 0
+max_points = (lenght * (lenght+1))/2
+
+for l in range(lenght):
+    if s[l] in 'AEIOU':
+        kev_points += lenght - l
+
+stu_points = max_points - kev_points
+
+if kev_points > stu_points:
+    print "Kevin", kev_points
+elif kev_points < stu_points:
+    print "Stuart", stu_points
+else:
+    print "Draw"
