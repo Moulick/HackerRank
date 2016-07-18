c685207a215276e02c542a1d21ddaedb6b4f381b
Moulick <byesahil+github@gmail.com>
2016-07-18 14:55:04 +0530

    ?

--- Sets/Set .add().py
+++ Sets/Set .add().py
@@ -1,7 +1,7 @@
-#Moulick
+# Moulick
 s = input()
 x = set()
-#used while so that even if total is more than given number
+# used while so that even if total is more than given number
 while True:
     try:
         x.add(input().upper())
